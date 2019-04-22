4dd3b02f9077eb7616f36f92704f557d47ff1ca6
yokesh <yokeshthirumoorthi@gmail.com>
2019-04-22 03:56:17 -0700

    connect to database

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("New node project")
+const { Client } = require('pg')
+
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'phoenix_react_curated_list_dev',
+  password: 'postgres',
+  port: 5432,
+})
+client.connect()
+
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
