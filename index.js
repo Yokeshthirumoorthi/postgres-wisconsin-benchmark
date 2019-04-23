63e61786911ae7875eb3d2198a91f67dc7a24801
yokesh <yokeshthirumoorthi@gmail.com>
2019-04-22 21:23:06 -0700

    add some comments

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const { Client } = require('pg')
 const fs = require('fs')
 
+/**
+ * Project Constants
+ */
 const TUP_COUNT = 50; // Number of tuples in result relation
 
+/**
+ * POSTGRES SCRIPTS TO
+ *  1. DROP TABLE
+ *  2. CREATE TABLE
+ *  3. INSERT ROW
+ */
 const DROP_TENKTUP1_TABLE = `DROP TABLE IF EXISTS TENKTUP1`;
 const CREATE_TENKTUP1_TABLE = `CREATE TABLE IF NOT EXISTS TENKTUP1
                 (   
@@ -43,6 +52,9 @@ const INSERT_TENKTUP1_ROW = `INSERT INTO TENKTUP1(
     string4
     ) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16)`;
 
+/**
+ * HELPER FUNCTIONS / UTIL FUNCTIONS 
+ */
 // Ref : https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -63,6 +75,15 @@ function shuffle(array) {
     return array;
 }
 
+
+const storeData = (data, path) => {
+    try {
+      fs.writeFileSync(path, JSON.stringify(data))
+    } catch (err) {
+      console.error(err)
+    }
+  };
+  
 const range = (tupCount) => [...Array(tupCount).keys()];
 const mod = (range, divisor) => range.map(x => x % divisor);
 // const modString = (range, divisor) => range.map(x => x % divisor);
@@ -80,23 +101,45 @@ const cyclicStrings = (range, divisor, strings) => {
     return range.map(i => strings[i % divisor].concat(xChars.join``));
 }; 
 
+
+/**
+ * GENERATE DATA
+ */
+
+// candidate key, unique, random order
 const unique1 = shuffle(range(TUP_COUNT));
+// primary key, unique, sequential
 const unique2 = range(TUP_COUNT);
-const two = mod(unique2, 2);
-const four = mod(unique2, 4);
-const ten = mod(unique2, 10);
-const twenty = mod(unique2, 20);
-const onePercent = mod(unique2, 100);
-const tenPercent = mod(unique2, 10);
-const twentyPercent = mod(unique2, 5);
-const fiftyPercent = mod(unique2, 2);
+// (unique1 mod 2)
+const two = mod(unique1, 2);
+// (unique1 mod 4)
+const four = mod(unique1, 4);
+// (unique1 mod 10)
+const ten = mod(unique1, 10);
+// (unique1 mod 20)
+const twenty = mod(unique1, 20);
+// (unique1 mod 100)
+const onePercent = mod(unique1, 100);
+// (unique1 mod 10)
+const tenPercent = mod(unique1, 10);
+// (unique1 mod 5)
+const twentyPercent = mod(unique1, 5);
+// (unique1 mod 2)
+const fiftyPercent = mod(unique1, 2);
 const unique3 = shuffle(range(TUP_COUNT));
+// (onePercent * 2)
 const evenOnePercent = onePercent.map(x => x * 2);
+// (onePercent * 2)+1
 const oddOnePercent = onePercent.map(x => (x * 2) + 1);
+// candidate key
 const stringu1 = unique2.map(generateUniqueString);
+// candidate key
 const stringu2 = [...stringu1].sort();
 const string4 = cyclicStrings(unique2, 4, ["AAAA", "HHHH", "OOOO", "VVVV"]);
 
+/**
+ * GENERATE TUPLE
+ */
 const generateTuple = (primaryKey, index) => [
     unique1[index], 
     primaryKey,
@@ -116,16 +159,11 @@ const generateTuple = (primaryKey, index) => [
     string4[index]
 ];
 
-const storeData = (data, path) => {
-  try {
-    fs.writeFileSync(path, JSON.stringify(data))
-  } catch (err) {
-    console.error(err)
-  }
-}
-
 const dataset = unique2.map(generateTuple);
 
+/**
+ * INSERT DATA INTO POSTGRES
+ */
 const client = new Client({
     user: 'postgres',
     host: 'localhost',
